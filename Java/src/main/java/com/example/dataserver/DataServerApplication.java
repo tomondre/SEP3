215bdb34afbe6d872a6b4dc9e040233d1a0fb95d
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-10-27 15:17:43 +0200

    Initial commit

---
+++ Java/src/main/java/com/example/dataserver/DataServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataServerApplication.class, args);
+    }
+
+}
