98e167369be9f7a83d7dfa95a15fa57b7581a908
Tabita <304181@via.dk>
2021-11-10 13:13:06 +0100

    Add provider and validation

---
+++ CSharp/BusinessLogic/BusinessLogic/Shared/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.FileSystemGlobbing;
+
+namespace BusinessLogic.Shared
+{
+    public class Validator
+    {
+        public bool isValidCvr(int cvr)
+        {
+            return cvr is >= 10000000 and <= 99999999;
+        }
+        
+        public bool isValidPostCode(int postCode)
+        {
+            return postCode is >= 1000 and <= 9999;
+        }
+        
+        public static bool isValidPassword(string password)
+        {
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,14}$");
+            Match match = regex.Match(password);
+            return match.Success;
+        }
+        
+        public static bool isValidEmail(string email)
+        {
+            Regex regex = new Regex( @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                                     @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + 
+                                     @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            Match match = regex.Match(email);
+            return match.Success;
+        }
+        
+        
+    }
+    
+}
--- CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Provider.cs
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Provider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using RiskFirst.Hateoas.Models;
 
 namespace GrpcFileGeneration.Models
@@ -7,14 +8,28 @@ namespace GrpcFileGeneration.Models
     {
         private Dictionary<string, Link> _links;
         public int Id { get; set; }
+        [Required, MaxLength(50)]
         public string CompanyName { get; set; }
+        [Required]
+        [Range(10000000,99999999)]
         public int Cvr { get; set; }
+        [Required, MaxLength(20)]
         public string PhoneNumber { get; set; }
+        [Required, MaxLength(500)]
         public string Description { get; set; }
+        [Required, MaxLength(50)]
         public string Street { get; set; }
+        [Required, MaxLength(5)]
         public string StreetNumber { get; set; }
+        [Required]
+        [Range(1000,9999)]
         public int PostCode { get; set; }
+        [Required, MaxLength(50)]
         public string City { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
 
         public void AddLink(string id, Link link)
         {
