7872c5c211b3aba2c1369a84e7028ddbc190300a
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-11 08:44:53 +0100

    Model class for the categories created. Supports Heateos links as well

---
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Category.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using RiskFirst.Hateoas.Models;
+
+namespace GrpcFileGeneration.Models
+{
+    public class Category : ILinkContainer
+    {
+        private Dictionary<string, Link> links;
+        public int Id { set; get; }
+        [Required(ErrorMessage = "Please add a category name")]
+        public string CategoryName { get; set; }
+
+
+        public Category()
+        {
+            Links = new Dictionary<string, Link>();
+        }
+        
+        public void AddLink(string id, Link link)
+        {
+            Links.Add(id, link);
+        }
+        
+        public Dictionary<string, Link> Links
+        {
+            get => links;
+            set => links = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {CategoryName}";
+        }
+    }
+}
