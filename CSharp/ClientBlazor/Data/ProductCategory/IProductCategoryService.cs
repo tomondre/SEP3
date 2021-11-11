0b3372170ecc0b0f2c4633001c40e5a04b8e5a82
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-11 08:50:43 +0100

    Rest services client side implemented

---
+++ CSharp/ClientBlazor/Data/ProductCategory/IProductCategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GrpcFileGeneration.Models;
+
+namespace ClientBlazor.Data.ProductCategory
+{
+    public interface IProductCategoryService
+    {
+        Task<Category> AddProductCategoryAsync(Category category);
+        Task<IList<Category>> GetAllCategoriesAsync();
+        Task<Category> EditProductCategoryAsync(Category category);
+        Task DeleteProductCategoryAsync(int id);
+    }
+}
---
+++ CSharp/ClientBlazor/Data/ProductCategory/ProductCategoryService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GrpcFileGeneration.Models;
+
+namespace ClientBlazor.Data.ProductCategory
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        private HttpClient client;
+        private string uri;
+
+        public ProductCategoryService(HttpClient client)
+        {
+            this.client = client;
+            uri = "https://localhost:5001/Categories";
+        }
+        
+        public async Task<Category> AddProductCategoryAsync(Category category)
+        {
+            var categoryAsJson = JsonSerializer.Serialize(category);
+            var stringContent = new StringContent(categoryAsJson, Encoding.UTF8, "application/json");
+            var httpResponseMessage = await client.PostAsync(uri, stringContent);
+            CheckException(httpResponseMessage);
+            var readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+            var deserialize = JsonSerializer.Deserialize<Category>(readAsStringAsync, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            return deserialize;
+        }
+
+        public async Task<IList<Category>> GetAllCategoriesAsync()
+        {
+            var httpResponseMessage = await client.GetAsync(uri);
+            CheckException(httpResponseMessage);
+            var readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+            var categories = JsonSerializer.Deserialize<IList<Category>>(readAsStringAsync, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            return categories;
+        }
+
+        public async Task<Category> EditProductCategoryAsync(Category category)
+        {
+            var categoryAsJson = JsonSerializer.Serialize(category);
+            var stringContent = new StringContent(categoryAsJson, Encoding.UTF8, "application/json");
+            var httpResponseMessage = await client.PutAsync(uri, stringContent);
+            CheckException(httpResponseMessage);
+            var readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+            var deserialize = JsonSerializer.Deserialize<Category>(readAsStringAsync, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            
+            return deserialize;
+        }
+
+        public async Task DeleteProductCategoryAsync(int id)
+        {
+            var httpResponseMessage = await client.DeleteAsync(uri + $"/{id}");
+            CheckException(httpResponseMessage);
+        }
+        
+        private void CheckException(HttpResponseMessage task)
+        {
+            if (!task.IsSuccessStatusCode)
+            {
+                throw new Exception($"Code: {task.StatusCode}, {task.ReasonPhrase} ");
+            }
+        }
+    }
+}
