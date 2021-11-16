8ec8ca056ea0ab1bf8848d91e214e421d1daf9e2
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-16 12:53:54 +0100

    Experience model classes with abstraction created

---
+++ CSharp/BusinessLogic/BusinessLogic/Model/Experiences/ExperienceModel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BusinessLogic.Networking.Experiences;
+using GrpcFileGeneration.Models;
+
+namespace BusinessLogic.Model.Experiences
+{
+    public class ExperienceModel : IExperienceModel
+    {
+        private IExperienceNet network;
+
+        public ExperienceModel(IExperienceNet network)
+        {
+            this.network = network;
+        }
+        
+        public async Task<Experience> AddExperienceAsync(GrpcFileGeneration.Models.Experience experience)
+        {
+            return await network.AddExperienceAsync(experience);
+        }
+
+        public async Task<IList<Experience>> GetAllProviderExperiencesAsync(Provider provider)
+        {
+            return await network.GetAllProviderExperiencesAsync(provider);
+        }
+
+        public async Task<IList<Experience>> GetAllWebShopExperiencesAsync()
+        {
+            return await network.GetAllWebShopExperiencesAsync();
+        }
+    }
+}
