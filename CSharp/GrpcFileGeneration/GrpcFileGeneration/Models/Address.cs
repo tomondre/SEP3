4484886f9835ecab46f802f2e4038865f294d6b7
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-16 09:29:52 +0100

    Address validation annotations

--- CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Address.cs
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Address.cs
@@ -1,12 +1,19 @@
-﻿namespace GrpcFileGeneration.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcFileGeneration.Models
 {
     public class Address
     {
+        [Required, MaxLength(50)]
         public string Street { get; set; }
+        [Required, MaxLength(5)]
         public string StreetNumber { get; set; }
+        [Required]
+        [Range(1000,9999)]
         public int PostCode { get; set; }
+        [Required, MaxLength(50)]
         public string City { get; set; }
-
+        
         public Address()
         {
         }
