1ce0459553c8b8906c0383de8b0aed3c787a9274
Tabita <304181@via.dk>
2021-11-17 09:25:32 +0100

    create network part in business logic for add experience

--- CSharp/BusinessLogic/BusinessLogic/Networking/Experiences/ExperienceNet.cs
+++ CSharp/BusinessLogic/BusinessLogic/Networking/Experiences/ExperienceNet.cs
@@ -1,15 +1,28 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading.Tasks;
 using GrpcFileGeneration.Models;
+using Networking.Experience;
 
 namespace BusinessLogic.Networking.Experiences
 {
     public class ExperienceNet : IExperienceNet
     {
-        private ExperienceSer
-        public Task<Experience> AddExperienceAsync(Experience experience)
+        private ExperienceService.ExperienceServiceClient client;
+
+        public ExperienceNet(ExperienceService.ExperienceServiceClient client)
         {
-            throw new System.NotImplementedException();
+            this.client = client;
+        }
+
+        public async Task<Experience> AddExperienceAsync(Experience experience)
+        {
+            var serialize = JsonSerializer.Serialize(experience);
+            var protobufMessage = await client.addExperienceAsync(new ProtobufMessage(){MessageOrObject = serialize});
+            return JsonSerializer.Deserialize<Experience>(protobufMessage.MessageOrObject, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
         }
 
         public Task<IList<Experience>> GetAllProviderExperiencesAsync(Provider provider)
