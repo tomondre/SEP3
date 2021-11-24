5faf4c1dd49f5667cbfd7e4f6d2bbb1728afb4b5
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-24 08:02:19 +0100

    Customer and Provider now inherit from the user class better design

--- CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Customer.cs
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Customer.cs
@@ -2,30 +2,19 @@
 
 namespace GrpcFileGeneration.Models
 {
-    public class Customer
+    public class Customer : User
     {
-        public int? Id { get; set; }
         [Required, MaxLength(50)]
         public string FirstName { get; set; }
         [Required, MaxLength(50)]
         public string LastName { get; set; }
-        
-        [Required, EmailAddress]
-        public string Email { get; set; }
-        
+
         [Required, MaxLength(20)]
         public string PhoneNumber { get; set; }
         
-        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,14}$",ErrorMessage =
-            "The password must be between 8 (included) and 14 (included) characters, " +
-            "contain at least one number" +
-            "contain at least one upper case character" +
-            "contain at least one lower case character")]
-        public string Password { get; set; }
         [Required]
         public Address Address { get; set; }
 
-        public string Token { set; get; }
         public Customer()
         {
             Address = new Address();
--- CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Provider.cs
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Provider.cs
@@ -5,10 +5,10 @@ using RiskFirst.Hateoas.Models;
 
 namespace GrpcFileGeneration.Models
 {
-    public class Provider : ILinkContainer
+    public class Provider : User, ILinkContainer
     {
         public Dictionary<string, Link> Links { set; get; }
-        public int Id { get; set; }
+        
         [Required, MaxLength(50)]
         public string CompanyName { get; set; }
         [Required]
@@ -18,21 +18,11 @@ namespace GrpcFileGeneration.Models
         public string PhoneNumber { get; set; }
         [Required, MaxLength(500)]
         public string Description { get; set; }
-        [Required, EmailAddress]
-        public string Email { get; set; }
-        public bool IsApproved { set; get; }
         
-        [Required]
-        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z]).{8,14}$",ErrorMessage =
-            "The password must be between 8 (included) and 14 (included) characters,\n " +
-            "contain at least one number\n" +
-            "contain at least one upper case character\n" +
-            "contain at least one lower case character")]
-        public string Password { get; set; }
+        public bool IsApproved { set; get; }
 
         public Address Address { get; set; }
 
-        public string Token { set; get; }
 
         public Provider()
         {
@@ -43,5 +33,6 @@ namespace GrpcFileGeneration.Models
         {
             Links = new Dictionary<string, Link>();
         }
+        
     }
 }
