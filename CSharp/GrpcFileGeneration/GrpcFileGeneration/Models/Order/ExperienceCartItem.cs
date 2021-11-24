5e5905c678283de3a12c0810e5e636709c7e2e28
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-11-24 13:17:36 +0100

    Order. proto now contains up to date objects + creating converters for gRPC objects

--- CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Order/ExperienceCartItem.cs
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Order/ExperienceCartItem.cs
@@ -1,3 +1,5 @@
+using Networking.Order;
+
 namespace GrpcFileGeneration.Models.Order
 {
     public class ExperienceCartItem
@@ -10,6 +12,31 @@ namespace GrpcFileGeneration.Models.Order
         public ExperienceCartItem()
         {
             Experience = new Experience();
+        }
+
+        public ExperienceCartItem(OrderItemMessage item)
+        {
+            Experience = new Experience
+            {
+                Id = item.Id,
+                Description = item.Description,
+                Picture = item.Picture,
+                Name = item.Name,
+                Price = item.Price,
+            };
+            Quantity = item.Quantity;
+        }
+
+        public OrderItemMessage ToMessage()
+        {
+            return new OrderItemMessage
+            {
+                Description = Experience.Description,
+                Name = Experience.Name,
+                Picture = Experience.Picture,
+                Price = Experience.Price,
+                Quantity = Quantity
+            };
         }
     }
 }
--- CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Order/Order.cs
+++ CSharp/GrpcFileGeneration/GrpcFileGeneration/Models/Order/Order.cs
@@ -1,3 +1,5 @@
+using Networking.Order;
+
 namespace GrpcFileGeneration.Models.Order
 {
     public class Order
@@ -14,6 +16,32 @@ namespace GrpcFileGeneration.Models.Order
             Customer = new();
             ShoppingCart = new();
             CreditCard = new();
+        }
+
+        public Order(OrderMessage order)
+        {
+            Comment = order.Comment;
+            Customer = new Customer() {Id = order.CustomerId};
+            Id = order.Id;
+            ShoppingCart = new ShoppingCart();
+            foreach (var orderItemMessage in order.Items)
+            {
+                ShoppingCart.ShoppingCartItems.Add(new ExperienceCartItem(orderItemMessage));
+            }
+        }
+
+        public OrderMessage ToMessage()
+        {
+            OrderMessage message = new OrderMessage
+            {
+                Comment = Comment,
+                Total = ShoppingCart.OrderTotal,
+            };
+            foreach (var item in ShoppingCart.ShoppingCartItems)
+            {
+                message.Items.Add(item.ToMessage());
+            }
+            return message;
         }
 
         public void RemoveCreditCardInformation()
