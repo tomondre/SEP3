e2d437e61efa8c749490d4500b1d6a8b6c803875
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-25 17:21:57 +0100

    Make use of the ToMessage method

--- CSharp/BusinessLogic/BusinessLogic/Networking/Customers/CustomerNet.cs
+++ CSharp/BusinessLogic/BusinessLogic/Networking/Customers/CustomerNet.cs
@@ -1,24 +1,27 @@
 ﻿using System.Text.Json;
 using System.Threading.Tasks;
 using GrpcFileGeneration.Models;
+using Networking.Address;
 using Networking.Customer;
+using Networking.User;
 
 namespace BusinessLogic.Networking.Customers
 {
     public class CustomerNet : ICustomerNet
     {
-
         private CustomerService.CustomerServiceClient client;
 
         public CustomerNet(CustomerService.CustomerServiceClient client)
         {
             this.client = client;
         }
+
         public async Task<Customer> CreateCustomerAsync(Customer customer)
         {
-            var serialize = JsonSerializer.Serialize(customer);
-            var protobufMessage = await client.createCustomerAsync(new ProtobufMessage(){MassageOrObject = serialize});
-            return JsonSerializer.Deserialize<Customer>(protobufMessage.MassageOrObject, new JsonSerializerOptions(){PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+            CustomerMessage customerMessage = new CustomerMessage(customer.ToMessage());
+            var protobufMessage = await client.CreateCustomerAsync(customerMessage);
+            //TODO finish after testing
+            return null;
         }
     }
 }
