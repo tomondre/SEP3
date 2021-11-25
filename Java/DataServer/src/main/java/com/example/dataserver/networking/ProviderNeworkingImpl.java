ebf5036fbecf2baa83fd3a8d5b8d522a91c0ad93
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-25 17:31:08 +0100

    create provider tested

--- Java/DataServer/src/main/java/com/example/dataserver/networking/ProviderNeworkingImpl.java
+++ Java/DataServer/src/main/java/com/example/dataserver/networking/ProviderNeworkingImpl.java
@@ -5,15 +5,16 @@ import com.example.dataserver.models.Provider;
 import com.google.gson.Gson;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import networking.provider.ProtobufMessage;
-import networking.provider.ProtobufProviderServiceGrpc;
+import networking.provider.ProviderMessage;
+import networking.provider.ProviderServiceGrpc;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 
 import java.util.ArrayList;
 
 @GrpcService
-public class ProviderNeworkingImpl extends ProtobufProviderServiceGrpc.ProtobufProviderServiceImplBase {
+public class ProviderNeworkingImpl extends ProviderServiceGrpc.ProviderServiceImplBase {
 
     private ProviderDAO model;
     private Gson gson;
@@ -26,7 +27,12 @@ public class ProviderNeworkingImpl extends ProtobufProviderServiceGrpc.ProtobufP
 
     @Async
     @Override
-    public void createProvider(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
+    public void createProvider(ProviderMessage request, StreamObserver<ProviderMessage> responseObserver) {
+        var provider = new Provider(request);
+        var user = provider.getUser();
+        user.setProvider(provider);
+
+        model.createProvider(user);
 //        Provider provider = gson.fromJson(request.getMassageOrObject(), Provider.class);
 //        model.createProvider(provider);
 //        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject("Success").build());
@@ -35,7 +41,7 @@ public class ProviderNeworkingImpl extends ProtobufProviderServiceGrpc.ProtobufP
 
     @Async
     @Override
-    public void getAllProviders(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
+    public void getAllProviders(ProviderMessage request, StreamObserver<ProviderMessage> responseObserver) {
 //        ArrayList<Provider> allProviders = model.getAllBy();
 //        String s = gson.toJson(allProviders);
 //        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject(s).build());
@@ -44,7 +50,7 @@ public class ProviderNeworkingImpl extends ProtobufProviderServiceGrpc.ProtobufP
 
     @Async
     @Override
-    public void getProviderById(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
+    public void getProviderById(ProviderMessage request, StreamObserver<ProviderMessage> responseObserver) {
 //        Provider providerById = model.getProviderById(Integer.parseInt(request.getMassageOrObject()));
 //        String providerJson = gson.toJson(providerById);
 //        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject(providerJson).build());
@@ -53,7 +59,7 @@ public class ProviderNeworkingImpl extends ProtobufProviderServiceGrpc.ProtobufP
 
     @Async
     @Override
-    public void editProvider(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
+    public void editProvider(ProviderMessage request, StreamObserver<ProviderMessage> responseObserver) {
 //        Provider provider = gson.fromJson(request.getMassageOrObject(), Provider.class);
 //        model.editProvider(provider);
 //        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject("Success").build());
@@ -62,14 +68,14 @@ public class ProviderNeworkingImpl extends ProtobufProviderServiceGrpc.ProtobufP
 
     @Async
     @Override
-    public void removeProvider(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
+    public void removeProvider(ProviderMessage request, StreamObserver<ProviderMessage> responseObserver) {
 //        model.removeProvider(Integer.parseInt(request.getMassageOrObject()));
 //        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject("Success").build());
 //        responseObserver.onCompleted();
     }
 
     @Override
-    public void getAllNotApprovedProviders(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
+    public void getAllNotApprovedProviders(ProviderMessage request, StreamObserver<ProviderMessage> responseObserver) {
 //        ArrayList<Provider> allNotApprovedProviders = model.getAllNotApprovedProviders();
 //        String json = gson.toJson(allNotApprovedProviders);
 //        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject(json).build());
