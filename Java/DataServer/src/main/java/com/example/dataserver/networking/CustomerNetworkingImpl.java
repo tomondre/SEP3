9c502031f1e97eb70333adbbfee78fc2abc662ca
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-25 17:23:05 +0100

    Uses the overloaded constructor for initializing a customer object from customer message

--- Java/DataServer/src/main/java/com/example/dataserver/networking/CustomerNetworkingImpl.java
+++ Java/DataServer/src/main/java/com/example/dataserver/networking/CustomerNetworkingImpl.java
@@ -1,12 +1,14 @@
 package com.example.dataserver.networking;
 
+import com.example.dataserver.models.Address;
 import com.example.dataserver.models.Customer;
+import com.example.dataserver.models.User;
 import com.example.dataserver.persistence.customer.CustomerDAO;
 import com.google.gson.Gson;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import networking.customer.CustomerMessage;
 import networking.customer.CustomerServiceGrpc;
-import networking.customer.ProtobufMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @GrpcService
@@ -21,11 +23,15 @@ public class CustomerNetworkingImpl extends CustomerServiceGrpc.CustomerServiceI
     }
 
     @Override
-    public void createCustomer(ProtobufMessage request, StreamObserver<ProtobufMessage> responseObserver) {
-//        Customer customer = gson.fromJson(request.getMassageOrObject(), Customer.class);
-//        Customer result = dao.createCustomer(customer);
-//        String s = gson.toJson(result);
-//        responseObserver.onNext(ProtobufMessage.newBuilder().setMassageOrObject(s).build());
+    public void createCustomer(CustomerMessage request, StreamObserver<CustomerMessage> responseObserver) {
+
+        var customer = new Customer(request);
+        var user = customer.getUser();
+        user.setCustomer(customer);
+
+        System.out.println(dao.createCustomer(user));
+
+        //        responseObserver.onNext(UserMessage.newBuilder().setMassageOrObject(s).build());
 //        responseObserver.onCompleted();
     }
 }
