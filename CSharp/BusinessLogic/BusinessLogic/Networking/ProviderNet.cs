b91648a473487afe5f53b50a5634f3f8747a6004
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-25 17:30:20 +0100

    made use of the ToMessage method

--- CSharp/BusinessLogic/BusinessLogic/Networking/ProviderNet.cs
+++ CSharp/BusinessLogic/BusinessLogic/Networking/ProviderNet.cs
@@ -5,76 +5,81 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using GrpcFileGeneration.Models;
+using Networking.Address;
 using Networking.Provider;
+using Networking.User;
 
 namespace BusinessLogic.Networking
 {
     public class ProviderNet : IProviderNet
     {
-        private ProtobufProviderService.ProtobufProviderServiceClient client;
+        private ProviderService.ProviderServiceClient client;
 
-        public ProviderNet(ProtobufProviderService.ProtobufProviderServiceClient client)
+        public ProviderNet(ProviderService.ProviderServiceClient client)
         {
             this.client = client;
         }
 
         public async Task CreateProvider(Provider provider)
         {
-            await client.createProviderAsync(new ProtobufMessage
-            {
-                MassageOrObject = serialize(provider)
-            });
+            var providerMessage = new ProviderMessage(provider.ToMessage());
+            var message = client.CreateProvider(providerMessage);
+            //TODO finish implementation
         }
 
         public async Task<IList<Provider>> GetAllProviders()
         {
-            IList<Provider> result;
-            var allProvidersAsync = await client.getAllProvidersAsync(new ProtobufMessage());
-            var massageOrObject = allProvidersAsync.MassageOrObject;
-            result = JsonSerializer.Deserialize<IList<Provider>>(massageOrObject, new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
-            return result;
+            // IList<Provider> result;
+            // var allProvidersAsync = await client.GetAllProvidersAsync(new ProtobufMessage());
+            // var massageOrObject = allProvidersAsync.MassageOrObject;
+            // result = JsonSerializer.Deserialize<IList<Provider>>(massageOrObject, new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // });
+            // return result;
+            return null;
         }
 
         public async Task<Provider> GetProviderById(int id)
         {
-            var providerByIdAsync =
-                await client.getProviderByIdAsync(new ProtobufMessage() {MassageOrObject = id.ToString()});
-            var massageOrObject = providerByIdAsync.MassageOrObject;
-            return JsonSerializer.Deserialize<Provider>(massageOrObject, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+            // var providerByIdAsync =
+            //     await client.getProviderByIdAsync(new ProtobufMessage() {MassageOrObject = id.ToString()});
+            // var massageOrObject = providerByIdAsync.MassageOrObject;
+            // return JsonSerializer.Deserialize<Provider>(massageOrObject, new JsonSerializerOptions
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // });
+            return null;
         }
 
         public async Task EditProvider(Provider provider)
         {
-            await client.editProviderAsync(new ProtobufMessage {MassageOrObject = serialize(provider)});
+            // await client.editProviderAsync(new ProtobufMessage {MassageOrObject = serialize(provider)});
         }
 
         public async Task DeleteProvider(int id)
         {
-            await client.removeProviderAsync(new ProtobufMessage {MassageOrObject = id.ToString()});
+            // await client.removeProviderAsync(new ProtobufMessage {MassageOrObject = id.ToString()});
         }
 
         public async Task<IList<Provider>> GetAllNotApprovedProviders()
         {
-            var allNotApprovedProvidersAsync = await client.getAllNotApprovedProvidersAsync(new ProtobufMessage());
-            string objectsAsJson = allNotApprovedProvidersAsync.MassageOrObject;
-            return JsonSerializer.Deserialize<IList<Provider>>(objectsAsJson, new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+            // var allNotApprovedProvidersAsync = await client.getAllNotApprovedProvidersAsync(new ProtobufMessage());
+            // string objectsAsJson = allNotApprovedProvidersAsync.MassageOrObject;
+            // return JsonSerializer.Deserialize<IList<Provider>>(objectsAsJson, new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // });
+            return null;
         }
-
+        
         private string serialize(Provider provider)
         {
-            return JsonSerializer.Serialize(provider, new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+            // return JsonSerializer.Serialize(provider, new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // });
+            return null;
         }
     }
 }
