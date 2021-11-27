fd252c49016f74681861f45922efd0678319e2e9
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-27 09:04:42 +0100

    namespace change and removed the unneeded classes

---
+++ CSharp/WebShop/Data/Customer/ICustomerNetwork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WebShop.Data.Customer
+{
+    public interface ICustomerNetwork
+    { 
+        Task CreateCustomerAsync(GrpcFileGeneration.Models.Customer customer);
+    }
+}
