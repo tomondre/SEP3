4cd91491feed31aaf5c36d94d5ea55456a11349c
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-27 09:12:20 +0100

    direct verification implemented

--- CSharp/ClientBlazor/Data/Provider/ProviderService.cs
+++ CSharp/ClientBlazor/Data/Provider/ProviderService.cs
@@ -1,30 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using ClientBlazor.Data.Authentication;
 using GrpcFileGeneration.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using ProviderObject = GrpcFileGeneration.Models.Provider;
 
-namespace ClientBlazor.Data
+
+namespace ClientBlazor.Data.Provider
 {
     public class ProviderService : IProviderService
     {
         private HttpClient client;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
         private string uri;
 
-        public ProviderService(HttpClient client)
+        public ProviderService(HttpClient client, AuthenticationStateProvider authenticationStateProvider)
         {
             this.client = client;
+            this.authenticationStateProvider = authenticationStateProvider;
             uri = "https://localhost:5001/Provider";
         }
 
-        public async Task CreateProvider(Provider provider)
+        public async Task CreateProvider(ProviderObject provider)
         {
             string providerAsJson = JsonSerializer.Serialize(provider);
             var stringContent = new StringContent(providerAsJson, Encoding.UTF8, "application/json");
             var httpResponseMessage = await client.PostAsync(uri, stringContent);
             CheckException(httpResponseMessage);
+            
+            var readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+            
+            var user = JsonSerializer.Deserialize<User>(readAsStringAsync, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            await ((CurrentAuthenticationStateProvider) authenticationStateProvider).ValidateUser(user);
         }
 
         public async Task<ProviderList> GetAllProviders()
@@ -40,12 +53,12 @@ namespace ClientBlazor.Data
             return providers;
         }
 
-        public Task<Provider> GetProviderById(int id)
+        public Task<ProviderObject> GetProviderById(int id)
         {
             throw new System.NotImplementedException();
         }
 
-        public async Task EditProvider(Provider provider)
+        public async Task EditProvider(ProviderObject provider)
         {
             var json = JsonSerializer.Serialize(provider);
             var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
@@ -53,7 +66,7 @@ namespace ClientBlazor.Data
             CheckException(httpResponseMessage);
         }
 
-        public async Task DeleteProvider(Provider provider)
+        public async Task DeleteProvider(ProviderObject provider)
         {
             var httpResponseMessage = await client.DeleteAsync($"{uri}/{provider.Id}");
             CheckException(httpResponseMessage);
