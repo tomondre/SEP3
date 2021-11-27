ff52ff7e9e38c4db6ad506bdb7f764d8cb4deea1
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-27 09:02:19 +0100

    after creating a logging it sends the object for validating and getting a session token

--- CSharp/WebShop/Data/Customer/CustomerNetwork.cs
+++ CSharp/WebShop/Data/Customer/CustomerNetwork.cs
@@ -1,37 +1,42 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using GrpcFileGeneration.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using WebShop.Data.Authentication;
 
-namespace WebShop.RestWebShop
+namespace WebShop.Data.Customer
 {
     public class CustomerNetwork : ICustomerNetwork
     {
         private HttpClient client;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
         private string uri;
 
-        public CustomerNetwork(HttpClient client)
+        public CustomerNetwork(HttpClient client, AuthenticationStateProvider authenticationStateProvider)
         {
             this.client = client;
+            this.authenticationStateProvider = authenticationStateProvider;
             uri = "https://localhost:5001/Customers";
         }
 
-        public async Task<Customer> CreateCustomerAsync(Customer customer)
+        public async Task CreateCustomerAsync(GrpcFileGeneration.Models.Customer customer)
         {
             string customerAsJson = JsonSerializer.Serialize(customer);
             var stringContent = new StringContent(customerAsJson, Encoding.UTF8, "application/json");
             var httpResponseMessage = await client.PostAsync(uri, stringContent);
+            
             CheckException(httpResponseMessage);
+            
             var json = await httpResponseMessage.Content.ReadAsStringAsync();
-            var deserialize = JsonSerializer.Deserialize<Customer>(json,
+            var deserialize = JsonSerializer.Deserialize<GrpcFileGeneration.Models.Customer>(json,
                 new JsonSerializerOptions()
                 {
                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
                 });
-            return deserialize;
+            await ((CurrentAuthenticationStateProvider) authenticationStateProvider).ValidateUser(deserialize);
         }
 
         private void CheckException(HttpResponseMessage task)
