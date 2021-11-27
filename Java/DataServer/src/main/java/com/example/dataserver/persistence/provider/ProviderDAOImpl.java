6fc64e9cf8929f759b710ed55a0b322f8ddfa467
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-27 09:09:03 +0100

    create returns user object

--- Java/DataServer/src/main/java/com/example/dataserver/persistence/provider/ProviderDAOImpl.java
+++ Java/DataServer/src/main/java/com/example/dataserver/persistence/provider/ProviderDAOImpl.java
@@ -8,45 +8,54 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 
 @Repository
-public class ProviderDAOImpl implements ProviderDAO {
+public class ProviderDAOImpl implements ProviderDAO
+{
 
-    private final UserRepository repo;
+  private final UserRepository repo;
 
-    @Autowired
-    public ProviderDAOImpl(UserRepository repo) {
-        this.repo = repo;
-    }
+  @Autowired
+  public ProviderDAOImpl(UserRepository repo)
+  {
+    this.repo = repo;
+  }
 
-    @Override
-    public void createProvider(User provider) {
-        repo.save(provider);
-    }
+  @Override
+  public User createProvider(User provider)
+  {
+    return repo.save(provider);
+  }
 
-    @Override
-    public ArrayList<User> getAllProviders() {
-        return repo.getAllByProvider_isApproved(true);
-    }
+  @Override
+  public ArrayList<User> getAllProviders()
+  {
+    return repo.getAllByProvider_isApproved(true);
+  }
 
-    @Override
-    public User getProviderById(int id) {
-        return repo.findById(id).orElseThrow();
-    }
+  @Override
+  public User getProviderById(int id)
+  {
+    return repo.findById(id).orElseThrow();
+  }
 
-    @Override
-    public void editProvider(User provider) {
-        if (repo.existsById(provider.getId()))
-            repo.save(provider);
-    }
+  @Override
+  public User editProvider(User provider)
+  {
+    if (repo.existsById(provider.getId()))
+      return repo.save(provider);
 
-    @Override
-    public void removeProvider(int id) {
-        repo.deleteById(id);
-    }
+    return null;
+  }
 
-    @Override
-    public ArrayList<User> getAllNotApprovedProviders() {
-        return repo.getAllByProvider_isApproved(true);
-    }
+  @Override
+  public void removeProvider(int id)
+  {
+    repo.deleteById(id);
+  }
 
+  @Override
+  public ArrayList<User> getAllNotApprovedProviders()
+  {
+    return repo.getAllByProvider_isApproved(false);
+  }
 
 }
