d4094cb76117eeb9cafc1225a0d8d7ab840e5e14
Khaled Hammoun <kh.hammoun@gmail.com>
2021-11-27 09:10:34 +0100

    return types edited

--- Java/DataServer/src/main/java/com/example/dataserver/persistence/provider/ProviderDAO.java
+++ Java/DataServer/src/main/java/com/example/dataserver/persistence/provider/ProviderDAO.java
@@ -6,10 +6,10 @@ import com.example.dataserver.models.User;
 import java.util.ArrayList;
 
 public interface ProviderDAO {
-    void createProvider(User provider);
+    User createProvider(User provider);
     ArrayList<User> getAllProviders();
     User getProviderById(int id);
-    void editProvider(User provider);
+    User editProvider(User provider);
     void removeProvider(int id);
     ArrayList<User> getAllNotApprovedProviders();
 }
