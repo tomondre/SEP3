cd6e788fe0573c88cc384ffc2ee9afabaf723a16
Tabita <304181@via.dk>
2021-12-08 14:53:09 +0100

    validation for credit card

--- CSharp/WebShop/Models/Orders/CreditCard.cs
+++ CSharp/WebShop/Models/Orders/CreditCard.cs
@@ -4,14 +4,19 @@ namespace WebShop.Models.Orders
 {
     public class CreditCard
     {
+        [Required]
+        [RegularExpression(@"^[0-9]{16}$", ErrorMessage = "Card number should be a 16 digit number")]
         public string Number { get; set; }
 
-        [Range(2, 2, ErrorMessage = "Month is a two digit number")]
+        [Required]
+        [Range(1, 12, ErrorMessage = "Month is a two digit number")]
         public int Month { get; set; }
 
-        [Range(2, 2, ErrorMessage = "Year is a two digit number")]
+        [Required]
+        [Range(21, 30, ErrorMessage = "Please add a valid expiry year")]
         public int Year { get; set; }
-        [RegularExpression(@"^[0-9]{3}$", ErrorMessage = "Month should be two digit number")]
+        [Required]
+        [RegularExpression(@"^[0-9]{3}$", ErrorMessage = "Cvv should be a three digit number")]
         public string Cvv { get; set; }
     }
 }
